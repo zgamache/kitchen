f72e748c1ed877ae01aceca425544e853b95c4cc
Zak Gamache <zak.gamache@gmail.com>
2018-10-28 14:51:01 -0600

    Add navigation drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,106 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom'
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  withStyles,
+} from '@material-ui/core'
 
 import { Main, Example } from './screens'
 
-function BasicExample() {
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Main,
+  },
+  {
+    path: '/example',
+    name: 'Example',
+    component: Example,
+  },
+]
+
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    flexShrink: 0,
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+  },
+})
+
+function App(props) {
+  const { classes } = props
+
   return (
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={classes.appBar}
+        >
+          <Toolbar>
+            <Typography>TEST</Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          className={classes.drawer}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <List>
+            {routes.map(route => (
+              <Link to={route.path} key={route.name}>
+                <ListItem button key={route.name}>
+                  <ListItemText primary={route.name} />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Switch>
+            {routes.map(route => (
+              <Route exact path={route.path} component={route.component} key={route.name} />
+            ))}
+          </Switch>
+        </main>
+      </div>
+    </Router>
+
+  /*
     <Router>
       <div>
         <ul>
@@ -22,7 +118,12 @@ function BasicExample() {
         <Route path="/example" component={Example} />
       </div>
     </Router>
+  */
   )
 }
 
-export default BasicExample
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App)
