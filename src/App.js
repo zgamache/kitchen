00ad4785f64cc2d2999acf94206004ae35ba466d
Zak Gamache <zak.gamache@gmail.com>
2018-10-28 12:14:05 -0600

    Initial router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import { Example } from './screens'
+import { Main, Example } from './screens'
 
-class App extends Component {
-  render() {
-    return (
-      <Example />
-    )
-  }
+function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/example">Create React App Example</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Main} />
+        <Route path="/example" component={Example} />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default BasicExample;
