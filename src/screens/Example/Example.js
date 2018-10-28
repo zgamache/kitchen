a39cd1403488940a2bf72c41b09d2b53dc05d6c2
Zak Gamache <zak.gamache@gmail.com>
2018-10-28 12:08:47 -0600

    Move create-react-app default App to src/screens/Example

--- src/screens/Example/Example.js
+++ src/screens/Example/Example.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Example.css';
 
-class App extends Component {
+class Example extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="Example">
+        <header className="Example-header">
+          <img src={logo} className="Example-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/Example.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className="Example-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -25,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Example;
